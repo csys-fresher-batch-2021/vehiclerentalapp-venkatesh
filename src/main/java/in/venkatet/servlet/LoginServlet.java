ce048d9d22320d95c43917e013fdf7eb93ee1ebd
Venkatesh Kumar K <venkisaravan007@gmail.com>
2021-05-21 12:04:42 +0530

    feature2BugFixing

--- src/main/java/in/venkatet/servlet/LoginServlet.java
+++ src/main/java/in/venkatet/servlet/LoginServlet.java
@@ -14,29 +14,25 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("######## Login Servlet ###########");
-		//Step 1: Get form values
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String username = request.getParameter("userName");
 		String password = request.getParameter("password");
-		//Step 2: call Service
+
 		boolean isValid = "admin".equals(username) && "admin".equals(password);
-		System.out.println("isValid ? " + isValid);
-		if(isValid) {
+
+		if (isValid) {
 			HttpSession session = request.getSession();
 			session.setAttribute("LOGGED_IN_USER", username);
 			session.setAttribute("ROLE", "ADMIN");
 			response.sendRedirect("addVehicleDetails.jsp");
-		}
-		else {
+		} else {
 			response.sendRedirect("adminLogin.jsp?errorMessage=Invalid Login Credentials");
 		}
-		
-	
-	}
 
+	}
 
 }
